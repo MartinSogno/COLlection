d21af432461401774aaf98aa1e9a34043d6fdc1b
Martin Sogno <martin.sogno@gmail.com>
2022-09-20 15:06:11 +0200

    loader

---
+++ loader.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from gpxcsv import gpxtolist
+import gzip
+import shutil
+
+
+COLUMNS_NAMES = {
+    'lat': 'latitude',
+    'lon': 'longitude',
+    'hr': 'heart_rate',
+    'ele': 'elevation',
+    'time':	'time'
+}
+
+def load(filename, is_compressed):
+    if is_compressed:
+        with gzip.open(f'gpx_files/{filename}.gpx.gz', 'rb') as f_compressed:
+            with open(f'gpx_files/{filename}.gpx', 'wb') as f_readable:
+                shutil.copyfileobj(f_compressed, f_readable)
+
+    ride = pd.DataFrame(gpxtolist(f'gpx_files/{filename}.gpx'))
+    ride['time'] = ride['time'].apply(pd.to_datetime)
+    columns = [column for column in ride.columns if column in COLUMNS_NAMES.values()]
+    return ride.rename(columns=COLUMNS_NAMES)[columns]
+
+
