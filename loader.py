dcdb67477ef130431b5ef8ed8b994e364a5f5670
Martin Sogno <martin.sogno@gmail.com>
2022-10-04 09:57:18 +0200

    add doc to loader and pretreat

--- loader.py
+++ loader.py
@@ -12,7 +12,9 @@ COLUMNS_NAMES = {
     'time':	'time'
 }
 
+
 def load(filename, is_compressed):
+    """Load a file in a readable DataFrame"""
     if is_compressed:
         with gzip.open(f'gpx_files/{filename}.gpx.gz', 'rb') as f_compressed:
             with open(f'gpx_files/{filename}.gpx', 'wb') as f_readable:
@@ -20,7 +22,6 @@ def load(filename, is_compressed):
 
     ride = pd.DataFrame(gpxtolist(f'gpx_files/{filename}.gpx'))
     ride['time'] = ride['time'].apply(pd.to_datetime)
+    ride = ride.rename(columns=COLUMNS_NAMES)
     columns = [column for column in ride.columns if column in COLUMNS_NAMES.values()]
-    return ride.rename(columns=COLUMNS_NAMES)[columns]
-
-
+    return ride[columns]
--- pretreat.py
+++ pretreat.py
@@ -4,19 +4,6 @@ from geopy import distance
 from scipy.ndimage import gaussian_filter1d
 
 
-def gpx_to_points(gpx, infos=['latitude', 'longitude', 'elevation', 'time', 'hr']):
-    return [
-        {info: getattr(point, info, None) for info in infos}
-        for track in gpx.tracks for segment in track.segments for point in segment.points
-    ]
-
-
-def get_duration(ride):
-    ride['point_duration'] = ride['time'].diff().apply(lambda p: p.seconds)
-    ride['total_duration'] = ride['point_duration'].cumsum()
-    return ride
-
-
 def calculate_intermediate_distance(point):
     return distance.distance(
         (point.latitude_shift, point.longitude_shift),
@@ -25,6 +12,7 @@ def calculate_intermediate_distance(point):
 
 
 def get_distance(ride):
+    """Get distance metrics of a ride"""
     ride['latitude_shift'] = ride['latitude'].shift().fillna(ride['latitude'][0])
     ride['longitude_shift'] = ride['longitude'].shift().fillna(ride['longitude'][0])
     ride['point_distance'] = ride.apply(calculate_intermediate_distance, axis=1)
@@ -33,7 +21,16 @@ def get_distance(ride):
     return ride.drop(columns=['latitude_shift', 'longitude_shift'])
 
 
+def get_duration(ride):
+    """Get duration metrics of a ride"""
+    ride['point_duration'] = ride['time'].diff().apply(lambda p: p.seconds)
+    ride['total_duration'] = ride['point_duration'].cumsum()
+    return ride
+
+
 def get_elevation_gain(ride):
+    """Get elevation gain metrics of a ride"""
+    # Some device only have int for elevation, smoother it to have more precise results
     ride['roll_elevation'] = gaussian_filter1d(ride['elevation'], 10)
     ride['point_elevation_diff'] = ride['roll_elevation'].diff()
     ride['point_elevation_gain'] = ride['point_elevation_diff'].apply(lambda point: max (0, point))
@@ -45,18 +42,21 @@ def get_elevation_gain(ride):
 
 
 def get_climb(ride):
+    """Get climb metrics of a ride"""
     ride['slope'] = (ride['point_elevation_diff'] / ride['point_distance']).apply(lambda s: min(max(s, -0.2), 0.2))
-    ride['climb_points'] = ride['slope'] * ride['point_elevation_gain']
+    ride['climb_difficulty'] = ride['slope'] * ride['point_elevation_gain']
     return ride
 
 
 def get_speeds(ride):
+    """Get speed metrics of a ride"""
     ride['speed'] = ride['point_distance'] / ride['point_duration']
     ride['vertical_speed'] = ride['point_elevation_diff'] / ride['point_duration']
     return ride
 
 
 def get_difficulty(ride):
+    """Get difficulty metrics of a ride"""
     # Assign a difficulty score to each of the points
     ride['point_difficulty'] = 0.001 * np.exp(20 * ride['slope']) * ride['point_distance']
     ride['total_difficulty'] = ride['point_difficulty'].cumsum()
@@ -64,6 +64,7 @@ def get_difficulty(ride):
 
 
 def treat_ride(ride):
+    """Get all the metics of a ride"""
     ride = get_distance(ride)
     ride = get_duration(ride)
     ride = get_elevation_gain(ride)
@@ -71,5 +72,4 @@ def treat_ride(ride):
     ride = get_speeds(ride)
     ride = get_difficulty(ride)
 
-    #ride = ride[ride['point_duration'] <= 10]
     return ride
